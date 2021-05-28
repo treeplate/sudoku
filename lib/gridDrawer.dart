a83cce13d642010150d68d3093b8d5f4c7fd21be
Ian Hickson <ian@hixie.ch>
2021-05-27 17:46:18 -0700

    lines and centers

--- lib/gridDrawer.dart
+++ lib/gridDrawer.dart
@@ -1,48 +1,56 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:sudoku/sevenSegmentDisplay.dart';
 
 class GridDrawer extends StatelessWidget {
-  const GridDrawer(this.grid, this.width, {Key? key}): super(key: key);
-  final List<List<int>> grid;
-  final int width;
-  int get height => grid.length ~/ width;
+  const GridDrawer({
+    Key? key,
+    required this.values,
+  }) : super(key: key);
+
+  final List<List<int>> values;
+
   @override
   Widget build(BuildContext context) {
-    //print("DRW");
     return CustomPaint(
-      painter: GridPainter(
-        width,
-        height,
-        grid,
-      ),
+      painter: GridPainter(values),
     );
   }
 }
 
 class GridPainter extends CustomPainter {
-  GridPainter(this.width, this.height, this.grid);
-  final int width;
-  final int height;
-  final List<List<int>> grid;
+  GridPainter(this.values)
+    : assert(math.sqrt(values.length) % 1 == 0),
+      assert(math.sqrt(math.sqrt(values.length)) % 1 == 0);
+
+  static final Paint thinLine = Paint()..style = PaintingStyle.stroke..color = Colors.black..strokeWidth = 1.0;
+  static final Paint thickLine = Paint()..style = PaintingStyle.stroke..color = Colors.black..strokeWidth = 4.0;
+  static final Paint redLine = Paint()..style = PaintingStyle.stroke..color = Colors.red;
+
+  final List<List<int>> values;
+
+  int get dim => math.sqrt(values.length).toInt();
+  int get blockSize => math.sqrt(dim).toInt();
 
   @override
-  bool shouldRepaint(GridPainter old) {
-    return width != old.width
-        || height != old.height
-        || grid != old.grid;
+  bool shouldRepaint(GridPainter oldDelegate) {
+    return values != oldDelegate.values;
   }
-
-  static final Paint blackLine = Paint()..style = PaintingStyle.stroke..color = Colors.black;
-  static final Paint redLine = Paint()..style = PaintingStyle.stroke..color = Colors.red;
 
   @override
   void paint(Canvas canvas, Size size) {
-    final double cellDim = size.shortestSide/width;
+    final double cellDim = size.shortestSide/dim;
     final double padding = cellDim / 10.0;
-    for (int y = 0; y < height; y += 1) {
-      for (int x = 0; x < width; x += 1) {
-        canvas.drawRect(Offset(x * cellDim, y * cellDim) & Size.square(cellDim), blackLine);
-        final List<int> centers = grid[x + (y * width)];
+    for (int x = 0; x < dim + 1; x += 1) {
+      canvas.drawLine(Offset(x * cellDim, 0), Offset(x * cellDim, cellDim * dim), x % blockSize == 0 ? thickLine : thinLine);
+    }
+    for (int y = 0; y < dim + 1; y += 1) {
+      canvas.drawLine(Offset(0, y * cellDim), Offset(cellDim * dim, y * cellDim), y % blockSize == 0 ? thickLine : thinLine);
+    }
+    for (int y = 0; y < dim; y += 1) {
+      for (int x = 0; x < dim; x += 1) {
+        final List<int> centers = values[x + (y * dim)];
         if (centers.isNotEmpty) {
           final double digitPadding = centers.length > 1 ? padding / (centers.length - 1) : 0.0;
           final double digitWidth = (cellDim - padding * 2.0) / centers.length;
